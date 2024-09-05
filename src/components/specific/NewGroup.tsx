e2d6167b98f23eb3442ae5b48b5f0b3c5412f8fb
Ajit Dhivar <ajitdhivar4d@gmail.com>
2024-09-05 11:06:40 +0530

    update

--- src/components/specific/NewGroup.tsx
+++ src/components/specific/NewGroup.tsx
@@ -73,7 +73,7 @@ const NewGroup = () => {
 
         {/* four */}
         <ul>
-          {users.map((user, i) => (
+          {users.map((user) => (
             <UserItem key={user} />
           ))}
         </ul>
